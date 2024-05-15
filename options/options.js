c01693389a742dfcdf0a849e4cb4268f89f9d76d
Crowdbotics <team@crowdbotics.com>
2024-05-15 23:05:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "curly-tooth-47975",
+  url: "https://curly-tooth-47975.botics.co",
+  api: "https://curly-tooth-47975.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
